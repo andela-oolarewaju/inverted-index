1bd9130676c582bb765f074988799b63c58a6a7d
andela-oolarewaju <olarewaju.oreoluwa@andela.com>
2015-09-22 09:37:13 +0100

    make changes to tests

--- jasmine/spec/invertedIndexTest.js
+++ jasmine/spec/invertedIndexTest.js
@@ -1,7 +1,8 @@
 "use strict";
 describe("IndexFile", function() {
+  var booksContent = indexCopy.booksJson("books.json");
   describe("Read book data", function() {
-    var booksContent = indexCopy.booksJson("books.json");
+    
     it("should not be empty", function() {
       expect(booksContent).not.toBe([]);
       expect(booksContent.length).not.toBe(0);
@@ -17,39 +18,41 @@ describe("IndexFile", function() {
       }
     });
   });
-});
 
-describe("Populate Index", function() {
-  var booksContent = indexCopy.booksJson("books.json");
-  it("should ensure that index is created", function() {
-    expect(getIndex).toBeDefined();   
+
+  describe("Populate Index", function() {
+    
+    it("should ensure that index is created", function() {
+      expect(getIndex).toEqual(jasmine.any(Object));   
+      expect(getIndex.length).not.toBe(0);
+    });
+
+    it("should ensure that index is correct", function() {
+      expect(getIndex.hobbit).toEqual([1]);
+    });
+    
+    it("should map index to its string keys", function() {
+      expect(booksContent[0].title).toEqual("Alice in Wonderland");
+      expect(booksContent[0].text).toEqual("Alice falls into a rabbit hole and enters a world full of imagination.");
+      expect(booksContent[1].title).toEqual("The Lord of the Rings: The Fellowship of the Ring.");
+      expect(booksContent[1].text).toEqual("An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring.");
+    });
   });
 
-  it("should ensure that index is correct", function() {
-    expect(getIndex.hobbit).toEqual([1]);
-  });
-  
-  it("should map index to its string keys", function() {
-    expect(booksContent[0].title).toEqual("Alice in Wonderland");
-    expect(booksContent[0].text).toEqual("Alice falls into a rabbit hole and enters a world full of imagination.");
-    expect(booksContent[1].title).toEqual("The Lord of the Rings: The Fellowship of the Ring.");
-    expect(booksContent[1].text).toEqual("An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring.");
-  });
-});
+  describe("Search index", function() {    
+    it("should be defined", function () {
+      expect(indexCopy.searchIndex).toBeDefined();
+    });
 
-describe("Search index", function() {    
-  it("should be defined", function () {
-    expect(indexCopy.searchIndex).toBeDefined();
-  });
+    it("should ensure index returns the correct results when searched.", function() {
+      expect(indexCopy.searchIndex("Alice")).toEqual([0]);
+      expect(indexCopy.searchIndex("of")).toEqual([0, 1]);
+      expect(indexCopy.searchIndex("Fellowship")).toEqual([1]);
+    });
 
-  it("should ensure index returns the correct results when searched.", function() {
-    expect(indexCopy.searchIndex("Alice")).toEqual([0]);
-    expect(indexCopy.searchIndex("of")).toEqual([0, 1]);
-    expect(indexCopy.searchIndex("Fellowship")).toEqual([1]);
-  });
-
-  it("should ensure searchIndex can handle a varied number of search terms as arguments.", function() {
-    var search = indexCopy.searchIndex("Alice", "Wonderland", "Fellowship");
-    expect(search).toEqual([0, 0 , 1]);
+    it("should ensure searchIndex can handle a varied number of search terms as arguments.", function() {
+      var search = indexCopy.searchIndex("Alice", "Wonderland", "Fellowship");
+      expect(search).toEqual([0, 0 , 1]);
+    });
   });
 });
